9cab3bec36cbdfd08d5fa479f5023391e419bd96
tb659 <474779508@qq.com>
2026-04-04 15:58:19 +0800

    feat(rime): 更新配置文件并重构Lua脚本功能
    - 在default.custom.yaml中添加了定制化配置信息，包括分布版本、
      生成器信息和修改时间等元数据
    
    - 配置了ASCII组合器的按键映射，支持Caps Lock、Shift等键位操作
    
    - 更新了输入法方案列表，包含tiger、tigress和PY_c等多个方案
    
    - 修改了菜单设置，调整了页面大小和选择标签配置
    
    - 在lua/number.lua中添加了源码注释信息，标注来源和修改者
    
    - 移除了数字转换结果中的标注文本，简化了输出格式
    
    - 新增了translator函数用于处理数字输入的转换逻辑
    
    - 在rime.lua中移除了大量原有的基础功能模块代码
    
    - 添加了新的模块引用，包括字符集过滤、Unicode显示、计算器
      和高级时间等功能模块

--- lua/number.lua
+++ lua/number.lua
@@ -1,4 +1,9 @@
-﻿local function splitNumPart(str)
+﻿--[[
+Source:http://98wb.ysepan.com/
+Modified by 惜洛
+--]]
+
+local function splitNumPart(str)
 	local part = {}
 	part.int, part.dot, part.dec = string.match(str, "^(%d*)(%.?)(%d*)")
 	return part
@@ -95,15 +100,31 @@ function number_translatorFunc(num)
 	local numberPart=splitNumPart(num)
 	local result={}
 	if numberPart.dot~="" then
-		table.insert(result,{number2cnChar(numberPart.int,0,{"万", "亿"},{"〇","一","十","点"})..number2zh(numberPart.dec,0),"〈数字小写〉"})
-		table.insert(result,{number2cnChar(numberPart.int,1,{"萬", "億"},{"〇","一","十","点"})..number2zh(numberPart.dec,1),"〈数字大写〉"})
+		table.insert(result,{number2cnChar(numberPart.int,0,{"万", "亿"},{"〇","一","十","点"})..number2zh(numberPart.dec,0),""})
+		table.insert(result,{number2cnChar(numberPart.int,1,{"萬", "億"},{"〇","一","十","点"})..number2zh(numberPart.dec,1),""})
+--		table.insert(result,{number2cnChar(numberPart.int,1,{"萬", "億"},{"〇","一","十","点"})..number2zh(numberPart.dec,1),"〔大写〕"})
 	else
-		table.insert(result,{number2cnChar(numberPart.int,0,{"万", "亿"},{"〇","一","十",""}),"〈数字小写〉"})
-		table.insert(result,{number2cnChar(numberPart.int,1,{"萬", "億"},{"零","壹","拾",""}),"〈数字大写〉"})
+		table.insert(result,{number2cnChar(numberPart.int,0,{"万", "亿"},{"〇","一","十",""}),""})
+		table.insert(result,{number2cnChar(numberPart.int,1,{"萬", "億"},{"零","壹","拾",""}),""})
 	end
-	table.insert(result,{number2cnChar(numberPart.int,1)..decimal_func(numberPart.dec,{[1]="角"; [2]="分"; [3]="厘"; [4]="毫"},{[0]="零"; "壹"; "贰"; "叁" ;"肆"; "伍"; "陆"; "柒"; "捌"; "玖"}),"〈金额大写〉"})
-	table.insert(result,{number2cnChar(numberPart.int,0)..decimal_func(numberPart.dec,{[1]="角"; [2]="分"; [3]="厘"; [4]="毫"},{[0]="〇"; "一"; "二"; "三" ;"四"; "五"; "六"; "七"; "八"; "九"}),"〈金额小写〉"})
+	table.insert(result,{number2cnChar(numberPart.int,1)..decimal_func(numberPart.dec,{[1]="角"; [2]="分"; [3]="厘"; [4]="毫"},{[0]="零"; "壹"; "贰"; "叁" ;"肆"; "伍"; "陆"; "柒"; "捌"; "玖"}),""})
+	table.insert(result,{number2cnChar(numberPart.int,0)..decimal_func(numberPart.dec,{[1]="角"; [2]="分"; [3]="厘"; [4]="毫"},{[0]="〇"; "一"; "二"; "三" ;"四"; "五"; "六"; "七"; "八"; "九"}),""})
 	return result
 end
 
--- print(#number_translatorFunc(3355.433))
+function translator(input, seg)
+	local str,num,numberPart
+	if string.match(input,"^(S+%d+)(%.?)(%d*)$")~=nil then
+		str = string.gsub(input,"^(%a+)", "")  numberPart=number_translatorFunc(str)
+		if #numberPart>0 then
+			for i=1,#numberPart do
+				yield(Candidate(input, seg.start, seg._end, numberPart[i][1],numberPart[i][2]))
+			end
+		end
+	end
+end
+
+
+return translator
+
+
